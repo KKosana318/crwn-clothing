0112a43c60ad53bfebe9f6feb734216fd457a003
Kesav Kosana <kkosana33@gmail.com>
2022-01-13 19:08:46 -0800

    add routing to menu items with useNavigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
